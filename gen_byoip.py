df6f37ac85165ece74a77d8f1ab84b2913fa7836
Tanya Flores <42090859+missTanya@users.noreply.github.com>
2020-11-02 18:13:43 +0800

    Add files via upload

---
+++ gen_byoip.py
@@ -0,0 +1,18 @@
+#! /usr/local/bin/python3
+
+import json
+from jinja2 import Environment, FileSystemLoader
+
+
+class main:
+    """This will generate Pillar structure for BYOIP by passing a csv file with parameters.
+    """
+
+    def pillarGenerator(object):
+
+
+
+
+
+if __name__ == '__main__':
+    main()
